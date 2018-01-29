d74ac17b40f5f5cbc86621b6d990bf1d44fb88c1
arunksalian <arun.salian@gmail.com>
2018-01-30 00:49:52 +0530

    Enums added

--- src/main/java/in/slin/app/core/data/ZodiacEnum.java
+++ src/main/java/in/slin/app/core/data/ZodiacEnum.java
@@ -7,8 +7,14 @@ public enum ZodiacEnum {
 	GEMINI ("Gemini", "Mithun", PlanetEnum.PLANET_MERCURY),
 	CANCER ("Cancer", "Kark", PlanetEnum.PLANET_MOON),
 	LEO ("Leo", "Singh", PlanetEnum.PLANET_SUN),
-	VIRGO ("Virgo", "Kanya", PlanetEnum.PLANET_MERCURY)
-	;
+	VIRGO ("Virgo", "Kanya", PlanetEnum.PLANET_MERCURY),
+	LIBRA ("Libra", "Tula", PlanetEnum.PLANET_VENUS),
+	SCORPIO("Scorpio", "Vrishchik", PlanetEnum.PLANET_MARS),
+	SAGITTARIUS("Sagittarius", "Dhanu", PlanetEnum.PLANET_JUPITER),
+	CAPRICORN("Capricorn", "Makar", PlanetEnum.PLANET_SATURN),
+	AQUARIUS ("Aquarius", "Kumbh", PlanetEnum.PLANET_SATURN),
+	PISCES ("Pisces", "Meen", PlanetEnum.PLANET_JUPITER);
+	
 	
 	String name;
 	String otherName;
